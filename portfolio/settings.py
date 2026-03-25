d3f53e31e040ffd137fe665efbdf668d844cdaff
JanushaNampally <nampallyjanusha@gmail.com>
2026-03-25 23:28:54 +0530

    Add deployment config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,8 +45,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ALLOWED_HOSTS = ['janushanampallyportfolio.onrender.com']
-
+ALLOWED_HOSTS = ['janushanampally-portfolio.onrender.com']
 # Application definition
 ROOT_URLCONF = "portfolio.urls"
 
@@ -116,5 +115,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
