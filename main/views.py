29ea34381e9ff838657f00f57c315d155f4f444d
JanushaNampally <nampallyjanusha@gmail.com>
2026-03-26 08:59:34 +0530

    minor changes

--- main/views.py
+++ main/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 
 def home(request):
+    return render(request, 'index.html')
+
+
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject') or "Portfolio Message"
+        message = request.POST.get('message')
+
+        full_message = f"From: {name} ({email})\n\n{message}"
+
+        try:
+            send_mail(
+                subject,
+                full_message,
+                settings.EMAIL_HOST_USER,
+                ['janushanampally24@gmail.com'],
+            )
+            messages.success(request, "✅ Message sent successfully!")
+
+        except Exception as e:
+            messages.error(request, "❌ Failed to send message")
+
+        return render(request, 'index.html')  # reload same page
+
     return render(request, 'index.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,3 +115,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'janushanampally24@gmail.com'
+EMAIL_HOST_PASSWORD = 'bkvq todr yjyd jakg'
